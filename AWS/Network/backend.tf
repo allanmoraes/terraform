25cbea8b3207c438b70267760dbd1ef4cbc1d34b
moraesallan <allan.moraes@stone.com.br>
2025-04-13 10:20:54 -0300

    ajustes

--- AWS/Network/backend.tf
+++ AWS/Network/backend.tf
@@ -5,6 +5,5 @@ terraform {
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "platform-terraform-locks"
-    role_arn       = "arn:aws:iam::278250362788:role/GitActionsNetwork"
   }
 }
